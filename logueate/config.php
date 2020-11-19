18692c5a7452527db7268d22c70331e6496670ad
Jymi3105 <jymi3105rhotmail.com>
2020-11-19 12:59:24 +0100

    a

--- logueate/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'jorge');
-   define('DB_PASSWORD', 'jymi');
-   define('DB_DATABASE', 'jymi');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
